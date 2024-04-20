2efc1108b27f1c41cc3a5cac68d8413c282cdc84
Camilo Ossa <ingcamilo.ossa@gmail.com>
2024-04-20 14:04:36 -0500

    Version 0.0.1 lista

---
+++ test/components/data/products.ts
@@ -0,0 +1,9 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffee Mug - Card - Sin imagen'
+}
+export const product2 = {
+  id: '1',
+  title: 'Coffee Mug - Card - Con imagen',
+  image: './coffee-mug.png'
+}
